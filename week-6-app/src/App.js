9bbe7771fc31367fcd5482d1f583d8579c87f904
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-17 16:24:08 +0100

    add main functionality to app

--- week-6-app/src/App.js
+++ week-6-app/src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import NameForm from "./NameForm";
+import Avatar from "./Avatar";
+import GithubImage from "./GithubImage";
 
 function App() {
-	return <div className="App"></div>;
+	const [githubUser, setGithubUser] = useState("thomas-galligan");
+	const [eyes, setEyes] = useState(3);
+	const [nose, setNose] = useState(2);
+	const [mouth, setMouth] = useState(1);
+	const [colour, setColour] = useState("ffa500");
+	const [githubUrl, setGithubUrl] = React.useState(
+		`https://api.github.com/users/${githubUser}`
+	);
+	return (
+		<div className={styles["root"]}>
+			<h1>Create your avatar!</h1>
+			<NameForm githubUser={githubUser} setGithubUser={setGithubUser} />
+			<Avatar
+				eyes={eyes}
+				setEyes={setEyes}
+				nose={nose}
+				setNose={setNose}
+				mouth={mouth}
+				setMouth={setMouth}
+				colour={colour}
+				setColour={setColour}
+			/>
+			<GithubImage
+				githubUser={githubUser}
+				githubUrl={githubUrl}
+				setGithubUrl={setGithubUrl}
+			/>
+		</div>
+	);
 }
 
 export default App;
