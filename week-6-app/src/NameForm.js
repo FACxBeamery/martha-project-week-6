af22719699f8c1b7f3f7b04cc1f80ce92b012a86
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-17 16:25:00 +0100

    get github username in form

---
+++ week-6-app/src/NameForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./NameForm.module.css";
+const NameForm = ({ githubUser, setGithubUser }) => {
+	return (
+		<form className={styles["text-input"]}>
+			<label htmlFor="githubUser" className={styles["form-label"]}>
+				Please enter your github username!
+			</label>
+			<input
+				type="text"
+				id="githubUser"
+				value={githubUser}
+				onChange={(e) => setGithubUser(e.target.value)}
+			></input>
+		</form>
+	);
+};
+
+export default NameForm;
