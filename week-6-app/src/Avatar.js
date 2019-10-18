728a841615016e4a9cd5c4c7ee2a25675a2b71ba
Martha Lambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-10-18 12:04:40 +0100

    refactor buttons into a component relates #6 #8

--- week-6-app/src/Avatar.js
+++ week-6-app/src/Avatar.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import styles from "./Avatar.module.css";
 import AvatarImage from "./AvatarImage";
+import ChangeFeatureButton from "./ChangeFeatureButton";
 
 const Avatar = ({
 	eyes,
@@ -15,25 +16,6 @@ const Avatar = ({
 	setGameProgress,
 	answer
 }) => {
-	const eyesNumbers = [1, 2, 3, 4, 5, 6, 7, 9, 10];
-	const noseNumbers = [2, 3, 4, 5, 6, 7, 8, 9];
-	const mouthNumbers = [1, 10, 11, 3, 5, 6, 7, 9];
-	const handleLeftClick = (event, value, setValue, numbersArray) => {
-		event.preventDefault();
-		let previousIndex = numbersArray.indexOf(value) - 1;
-		previousIndex =
-			previousIndex < 0 ? numbersArray.length - 1 : previousIndex;
-
-		setValue(numbersArray[previousIndex]);
-	};
-	const handleRightClick = (event, value, setValue, numbersArray) => {
-		event.preventDefault();
-		let previousIndex = numbersArray.indexOf(value) + 1;
-		previousIndex =
-			previousIndex > numbersArray.length - 1 ? 0 : previousIndex;
-		setValue(numbersArray[previousIndex]);
-	};
-
 	useEffect(() => {
 		setTimeout(500);
 		const newGameProgress = [
@@ -41,84 +23,69 @@ const Avatar = ({
 			nose === answer[1],
 			mouth === answer[2]
 		];
-		console.log("answer=", answer);
-		console.log("eyes", eyes, "eyes answer", answer[0]);
-		console.log("nose", nose, "nose answer", answer[1]);
-		console.log("mouth", mouth, "mouth answer", answer[2]);
 		setGameProgress(newGameProgress);
 	}, [eyes, nose, mouth, answer]);
 
+	const eyesNumbers = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+	const noseNumbers = [2, 3, 4, 5, 6, 7, 8, 9];
+	const mouthNumbers = [1, 10, 11, 3, 5, 6, 7, 9];
+
+	const featuresObj = {
+		eyes: {
+			value: eyes,
+			setFunction: setEyes,
+			array: eyesNumbers,
+			arrayIndex: 0,
+			emoji: "👀"
+		},
+		nose: {
+			value: nose,
+			setFunction: setNose,
+			array: noseNumbers,
+			arrayIndex: 1,
+			emoji: "👃"
+		},
+		mouth: {
+			value: mouth,
+			setFunction: setMouth,
+			array: mouthNumbers,
+			arrayIndex: 2,
+			emoji: "👄"
+		}
+	};
 	return (
 		<form className={styles["form-div"]}>
 			<div className={styles["buttons-div"]}>
-				<button
-					name="eyesLeft"
-					className={styles["arrow-left"]}
-					onClick={(event) =>
-						handleLeftClick(event, eyes, setEyes, eyesNumbers)
-					}
-					disabled={gameProgress[0]}
-				>
-					{gameProgress[0] ? "✅" : "👀 ⬅"}
-				</button>
-				<button
-					name="noseLeft"
-					className={styles["arrow-left"]}
-					onClick={(event) =>
-						handleLeftClick(event, nose, setNose, noseNumbers)
-					}
-					disabled={gameProgress[1]}
-				>
-					{gameProgress[1] ? "✅" : "👃 ⬅"}
-				</button>
-				<button
-					name="mouthLeft"
-					className={styles["arrow-left"]}
-					onClick={(event) =>
-						handleLeftClick(event, mouth, setMouth, mouthNumbers)
-					}
-					disabled={gameProgress[2]}
-				>
-					{gameProgress[2] ? "✅" : "👄 ⬅"}
-				</button>
+				{["eyes", "nose", "mouth"].map((currentFeature) => {
+					return (
+						<ChangeFeatureButton
+							key={currentFeature}
+							feature={currentFeature}
+							direction="Left"
+							featuresObj={featuresObj}
+							gameProgress={gameProgress}
+						></ChangeFeatureButton>
+					);
+				})}
 			</div>
 			<AvatarImage
 				eyes={eyes}
 				nose={nose}
 				mouth={mouth}
 				colour={colour.substring(1)}
-			/>
+			></AvatarImage>
 			<div className={styles["buttons-div"]}>
-				<button
-					name="eyesRight"
-					className={styles["arrow-right"]}
-					onClick={(event) =>
-						handleRightClick(event, eyes, setEyes, eyesNumbers)
-					}
-					disabled={gameProgress[0]}
-				>
-					{gameProgress[0] ? "✅" : "➡ 👀"}
-				</button>
-				<button
-					name="noseRight"
-					className={styles["arrow-right"]}
-					onClick={(event) =>
-						handleRightClick(event, nose, setNose, noseNumbers)
-					}
-					disabled={gameProgress[1]}
-				>
-					{gameProgress[1] ? "✅" : "➡ 👃"}
-				</button>
-				<button
-					name="mouthRight"
-					className={styles["arrow-right"]}
-					onClick={(event) =>
-						handleRightClick(event, mouth, setMouth, mouthNumbers)
-					}
-					disabled={gameProgress[2]}
-				>
-					{gameProgress[2] ? "✅" : "➡ 👄"}
-				</button>
+				{["eyes", "nose", "mouth"].map((currentFeature) => {
+					return (
+						<ChangeFeatureButton
+							key={currentFeature}
+							feature={currentFeature}
+							direction="Right"
+							featuresObj={featuresObj}
+							gameProgress={gameProgress}
+						></ChangeFeatureButton>
+					);
+				})}
 			</div>
 		</form>
 	);
